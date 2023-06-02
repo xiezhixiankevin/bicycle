9d3eb585bf9638456a4ea328eea112f6a63b7865
xiezhixian <268022625@qq.com>
2023-06-02 12:20:57 +0800

    new

--- bicycle/feign-api/src/main/java/cn/itcast/feign/pojo/Bicycle.java
+++ bicycle/feign-api/src/main/java/cn/itcast/feign/pojo/Bicycle.java
@@ -2,8 +2,10 @@ package cn.itcast.feign.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
@@ -19,6 +21,8 @@ import java.io.Serializable;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bicycle implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- bicycle/feign-api/src/main/java/cn/itcast/feign/pojo/TUser.java
+++ bicycle/feign-api/src/main/java/cn/itcast/feign/pojo/TUser.java
@@ -4,8 +4,10 @@ import cn.itcast.feign.pojo.pack.UserPack;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
@@ -21,6 +23,8 @@ import java.io.Serializable;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class TUser implements Serializable {
 
     private static final long serialVersionUID = 1L;
